9df2dd37e616247d06e8d16ce8643a6903f8ade0
johnnybravo916 <johnong916@gmail.com>
2017-04-03 09:25:31 -0500

    wireframe done for homepage

---
+++ gruntfile.js
@@ -0,0 +1,52 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+    sass: {
+      dist: {
+        options: {
+          sourcemap: 'none'
+        },
+        files: [{
+          expand: true,
+          cwd: 'sass',
+          src: ['**/*.scss'],
+          dest: 'css',
+          ext: '.css'
+      }]
+      }
+    },
+    cssmin: { // Begin CSS Minify Plugin
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'css',
+          src: ['**/*.css', '!*.min.css'],
+          dest: 'css',
+          ext: '.min.css'
+    }]
+      }
+    },
+    uglify: { // Begin JS Uglify Plugin
+      build: {
+        src: ['src/*.js'],
+        dest: 'js/script.min.js'
+      }
+    },
+		watch: {
+			css: {
+				files: '**/*.scss',
+				tasks: ['sass','cssmin']
+			},
+      js: {
+        files: '**/*.js',
+        tasks: ['uglify']
+      }
+		}
+	});
+	grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+  
+	grunt.registerTask('magic',['watch']);
+}
